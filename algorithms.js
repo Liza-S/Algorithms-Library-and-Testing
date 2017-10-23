200acd1e73c27654024671bb705fdd2efa52d221
Liza-S <lizash605@gmail.com>
2017-10-23 23:03:14 +0300

    Added recursiveLinearSearch

--- algorithms.js
+++ algorithms.js
@@ -19,3 +19,28 @@ function linearSearch(value, array)
     if (index < length) return index;          
     else return "Not found.";               
 }
+
+/*
+*	
+*	Linear search sequentially checks each element of the list until it finds an element that matches the target value, using recursion.
+*
+*	@param value - Value of the element which index should be found.
+*	@param array - Input array
+*
+*	Time complexity: O(n)	
+*/
+function recursiveLinearSearch(value,array) 
+{
+	var length  = array.length, index = 0;                                         
+
+	if (index>length){
+		return 'Not found.';
+
+	} else if (array[index]==value) {
+		return index;
+
+	} else {
+		return (recursiveLinearSearch(array,length,index+1,value));
+	}
+
+}
