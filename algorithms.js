922667e6a99338a89473fa3bc4649e10f688cbeb
Liza-S <lizash605@gmail.com>
2017-10-23 23:19:54 +0300

    Added binary search

--- algorithms.js
+++ algorithms.js
@@ -4,6 +4,7 @@
 *
 *	@param value - Value of the element which index should be found.
 *	@param array - Input array
+*	@returns Index of the element or 'Not found.' if not found.
 *
 *	Time complexity: O(n)	
 */
@@ -26,6 +27,7 @@ function linearSearch(value, array)
 *
 *	@param value - Value of the element which index should be found.
 *	@param array - Input array
+*	@returns Index of the element or 'Not found.' if not found.
 *
 *	Time complexity: O(n)	
 */
@@ -43,4 +45,28 @@ function recursiveLinearSearch(value,array)
 		return (recursiveLinearSearch(array,length,index+1,value));
 	}
 
+}
+
+/**
+* 	Searches for specific element in a given array using the binary search algorithm.
+*
+* 	@param array Input array.
+* 	@param value Value of the element which index should be found.
+* 	@returns Index of the element or 'Not found.' if not found.
+*	
+*	Time complexity: O(log N).
+*/
+function binarySearch(value, array)       
+{                                
+    var index = 0, length = array.length, middle; 
+                                 
+    while (index < length)                
+    {  
+    	middle = Math.floor((index+length)/2);
+       	if (value <= A[middle]) length = middle;
+       	else index = middle+1;
+    }
+   
+    if (A[ index ] === value) return index;     
+    else return 'Not found.';                 
 }
